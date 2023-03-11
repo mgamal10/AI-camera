1ea4ca9b8684ee4e60b9949997e49d8f2a5a8958
mgamal10 <127049129+mgamal10@users.noreply.github.com>
2023-03-11 14:38:17 +0200

    Update get_started.dart

--- lib/screens/get_started.dart
+++ lib/screens/get_started.dart
@@ -260,41 +260,41 @@ class _CarouselWithIndicatorState extends State<CarouselWithIndicatorDemo> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      CarouselSlider(
-        items: tips,
-        carouselController: _controller,
-        options: CarouselOptions(
-            //height: 400,
-            autoPlay: true,
-            aspectRatio: 1.4,
-            onPageChanged: (index, reason) {
-              setState(() {
-                _current = index;
-              });
-            }),
-      ),
-      Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: tips.asMap().entries.map((entry) {
-          return GestureDetector(
-            onTap: () => _controller.animateToPage(entry.key),
-            child: Container(
-              width: 12.0,
-              height: 12.0,
-              margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
-              decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: (Theme.of(context).brightness == Brightness.light
-                          ? Colors.white
-                          : Colors.black)
-                      .withOpacity(_current == entry.key ? 0.9 : 0.4)),
-            ),
-          );
-        }).toList(),
-          )
-        ],
-      ),
-    ]);
+    return Column(
+      children: [
+        CarouselSlider(
+          items: tips,
+          carouselController: _controller,
+          options: CarouselOptions(
+              //height: 400,
+              autoPlay: true,
+              aspectRatio: 1.4,
+              onPageChanged: (index, reason) {
+                setState(() {
+                  _current = index;
+                });
+              }),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: tips.asMap().entries.map((entry) {
+            return GestureDetector(
+              onTap: () => _controller.animateToPage(entry.key),
+              child: Container(
+                width: 12.0,
+                height: 12.0,
+                margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: (Theme.of(context).brightness == Brightness.light
+                            ? Colors.white
+                            : Colors.black)
+                        .withOpacity(_current == entry.key ? 0.9 : 0.4)),
+              ),
+            );
+          }).toList(),
+        )
+      ],
+    );
   }
 }
